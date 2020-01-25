60099788caf49fe1e9abeb3549effcf3a65b36e4
RyanStonebraker <ryanastonebraker@gmail.com>
2020-01-24 17:40:24 -0900

    Creates command and execute flask server

---
+++ src/server.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+from io import StringIO
+import sys
+
+app = Flask(__name__)
+
+@app.route("/execute")
+def execute():
+    command = "=".join(request.args).replace("\\n", "\n").replace("\\t", "\t")
+    old_stdout = sys.stdout
+    sys.stdout = StringIO()
+    output = sys.stdout
+    exec(command)
+    sys.stdout = old_stdout
+    return output.getvalue()
+
+
+@app.route("/")
+def main():
+    return "Welcome to my super secure site."
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
