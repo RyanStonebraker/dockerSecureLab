0acccdd4aa47703eb7467a54b4942f8abc6a05e2
RyanStonebraker <ryanastonebraker@gmail.com>
2020-01-25 12:44:06 -0900

    Adds volumes and loads from environment

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,9 @@ FROM ubuntu:latest
 
 RUN apt-get update -y && apt-get upgrade -y && apt-get install -y python3 python3-pip
 
-WORKDIR /app
-COPY src .
+WORKDIR /app/src
+COPY ./src /app/src
 
 RUN pip3 install -r requirements.txt
 
-CMD python3 server.py
-
-EXPOSE 8090
+CMD python3 /app/src/server.py
--- src/server.py
+++ src/server.py
@@ -1,23 +1,24 @@
 from flask import Flask, request
 from io import StringIO
 import sys
+import os
 
 app = Flask(__name__)
 
 @app.route("/execute")
 def execute():
     command = "=".join(request.args).replace("\\n", "\n").replace("\\t", "\t")
-    old_stdout = sys.stdout
+    save_out = sys.stdout
     sys.stdout = StringIO()
     output = sys.stdout
     exec(command)
-    sys.stdout = old_stdout
+    sys.stdout = save_out
     return output.getvalue()
 
 
 @app.route("/")
 def main():
-    return "Welcome to my super secure site."
+    return os.environ.get("SECRET_MESSAGE", "Welcome to my super secure site.")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="8090", debug=True)
